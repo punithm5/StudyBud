5ef318638e5e6a83bc8d7f2328e052739b9d4554
punithm5 <punithlokesh42@gmail.com>
2025-09-07 01:20:11 +0530

    07/09/2025

--- base/models.py
+++ base/models.py
@@ -4,3 +4,13 @@ from django.db import models
 “this class is a model, 
 map it to a database table, and give it ORM superpowers.”
 '''
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
