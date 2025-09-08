2b21bc79ba05f38989e3ef39defde4d937a55cd4
punithm5 <punithlokesh42@gmail.com>
2025-09-08 14:02:11 +0530

    08/09/2025

--- base/models.py
+++ base/models.py
@@ -3,6 +3,9 @@ from django.db import models
 (models.Model) tells Django 
 “this class is a model, 
 map it to a database table, and give it ORM superpowers.”
+
+models → module/package (django.db.models).
+Model → class inside that module.
 '''
 
 class Room(models.Model):
