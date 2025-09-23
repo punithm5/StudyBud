36f5ece43494eb5664c924964d232c591b96c2b7
punithm5 <punithlokesh42@gmail.com>
2025-09-23 17:59:22 +0530

    23/09/2025

--- base/models.py
+++ base/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 '''
 (models.Model) tells Django 
 “this class is a model, 
@@ -8,10 +10,17 @@ from django.some_module import SomeClassOrFunction
 models → module/package (django.db.models).
 Model → class inside that module.
 '''
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(null = True, blank=True)
+    description = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True) # Updates the timestamp every time the object is saved
     created = models.DateTimeField(auto_now_add=True) # Sets the timestamp once when the object is created
 
@@ -19,11 +28,11 @@ class Room(models.Model):
         return self.name
     
 class Message(models.Model):
-    # user
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name[:50]
+        return self.body[:50]
